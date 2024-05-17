04d8cae902d6f5eb77560812279a9694b53888bd
Lucas Ferreira <129186705+Lucasapn2@users.noreply.github.com>
2024-05-17 12:35:37 -0300

    update

--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('download-form').addEventListener('submit', function(event) {
-        document.getElementById('download-message').style.display = 'block';
-    });
-});
